e3a4c5d93b459cf9b24d341fa9801a78ff3e0a3d
Lokost39 <Lokost39@yandex.ru>
2018-12-31 15:23:19 +0400

    12

--- src/main/java/MLP.java
+++ src/main/java/MLP.java
@@ -10,7 +10,65 @@ public class MLP {
     private double[] answere = {0,1,1,0};
 
     public void init(){
+        for(int i = 0; i < wEH.length; i++){
+            for(int j = 0; j < wEH[i].length; j++){
+                wEH[i][j] = Math.random()*0.3 + 0.1;
+            }
+        }
+        for(int i = 0; i < wHO.length; i++){
 
+            wHO[i] = Math.random()*0.3 + 0.1;
+
+        }
+    }
+
+    public void countOuter(){
+        for(int i = 0; i < hidden.length; i++){
+            hidden[i] = 0;
+            for(int j = 0; j < enters.length; j++){
+                hidden[i] += enters[j] * wEH[i][j];
+            }
+
+            if(hidden[i] > 0.5) hidden[i] = 1; else hidden[i] = 0;
+        }
+        outer = 0;
+
+        for(int i = 0; i < enters.length; i++){
+            outer += hidden[i] * wHO[i];
+        }
+
+        if(outer > 0.5) outer = 1; else outer = 0;
+    }
+
+    public void stady(){
+        double[] err = new double[hidden.length];
+        double gError = 0;
+        do {
+            gError = 0;
+            for(int p = 0; p < patterns.length; p++){
+                for(int i = 0; i < enters.length; i++){
+                    enters[i] = patterns[p][i];
+                }
+                countOuter();
+                double lErr = answere[p] - outer;
+                gError += Math.abs(lErr);
+
+                for(int i = 0; i < hidden.length; i++){
+                    err[i] = lErr*wHO[i];
+                }
+
+                for(int i = 0; i < enters.length; i++){
+                    for(int j = 0; j < hidden.length; j++){
+                        wEH[i][j] += 0.1 * err[j] * enters[i];
+                                            }
+                }
+
+                for(int i = 0; i < hidden.length; i++){
+                    wHO[i] += 0.1 * lErr * hidden[i];
+                }
+            }
+
+        }while (gError != 0);
     }
 
     public MLP() {
@@ -19,5 +77,17 @@ public class MLP {
 
         this.wEH = new double[enters.length][hidden.length];
         this.wHO =  new double[hidden.length];
+
+
+        init();
+        stady();
+
+        for(int p = 0; p < patterns.length; p++) {
+            for (int i = 0; i < enters.length; i++) {
+                enters[i] = patterns[p][i];
+            }
+            countOuter();
+            System.out.println(outer);
+        }
     }
 }
